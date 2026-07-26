4c89dd1128a362ab2a48a73ea1f5cadf9d268fbc
Abdullah Waheed <abdullahwaheed372@gmail.com>
2026-07-26 19:27:03 +0500

    Search insert position solution

---
+++ 35-Search-Insert-position.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start = 0 ;
+        int end = nums.size()-1;
+        while(start <= end)
+        {
+           int  mid = (start+end)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid] > target)
+            {
+                end--;
+            }
+            else if(nums[mid] < target)
+            {
+                start++; 
+            }
+           
+        }
+         
+       return start++;
+    
+            
+        
+    }
+};
