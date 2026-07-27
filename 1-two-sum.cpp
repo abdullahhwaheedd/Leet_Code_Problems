ec7a4db72291d59d31000501bf41311814ad150c
Abdullah Waheed <abdullahwaheed372@gmail.com>
2026-07-27 12:11:49 +0500

    Two sum Solution

--- 1-two-sum.cpp
+++ 1-two-sum.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
        for(int i = 0 ; i < nums.size() ; i++)
        {
-        for(int j = i ; j < nums.size() - 1 ;j++)
+        for(int j = i+1 ; j < nums.size()  ;j++)
         {
-            if(nums[i]+nums[j+1] == target)
+            if(nums[i]+nums[j] == target)
             {
-                return {i,j+1};
+                return {i,j};
             }
         }
        }  
-     return {target};
+     return {};
     }
 };
