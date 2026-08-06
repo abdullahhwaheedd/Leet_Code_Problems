48858f2002330c79e62930f04e4d0884aaf690ca
Abdullah Waheed <abdullahwaheed372@gmail.com>
2026-08-06 12:32:32 +0500

    Valid Palindrome Solution

--- 125-Valid-Palindrome.cpp
+++ 125-Valid-Palindrome.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
 
-    bool isAlphanumeric(char ch)
-    {
-        if(ch >= '0' && ch <= '9' || tolower(ch) >= 'a' && tolower(ch) <= 'z')
-        {
-            return true;
-        }
-        return false;
-    }
+    
     bool isPalindrome(string s) {
         int st = 0;
         int end = s.length()-1;
         while(st < end)
         {
-            if(!isAlphanumeric(s[st]))
+            if(!isalnum(s[st]))
             {
                 st++;
                 continue;
             }
-            if(!isAlphanumeric(s[end]))
+            if(!isalnum(s[end]))
             {
                 end--;
                 continue;
